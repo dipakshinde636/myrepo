d25fd53bc8476c6b92d92a7f4d054d712e76f18a
D <D@DESKTOP-QMO6668>
2020-10-20 11:47:01 +0530

    3

--- Spring4MVCHelloWorldRestServiceDemo/src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
+++ Spring4MVCHelloWorldRestServiceDemo/src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
@@ -3,7 +3,7 @@ package com.websystique.springmvc.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping   ;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.websystique.springmvc.domain.Message;
